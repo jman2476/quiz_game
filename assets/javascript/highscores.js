4697ed82e11b05b83bada5963ff281c8ebf7795b
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-22 22:02:30 -0500

    removed console logs

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -1,12 +1,11 @@
 // retrieve the highscores array from localStorage, and output
 // it to the scores div
 var scoresArray = JSON.parse(localStorage.getItem('highscores'))
-console.log(scoresArray)
+
 
 // sort array by score, ignoring points, store as new variable
 scoresArray.sort((score1,score2) => (score1.score < score2.score) ? 1 : (score1.score > score2.score) ? -1 : 0);
 
-console.log(scoresArray)
 
 // Display the highscores as an ordered list, along with a button to restart the game
 function displayHighscores() {
