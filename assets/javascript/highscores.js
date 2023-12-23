ca55469fb4fc4059a7e9f5d887dcb2e1a0f724c8
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-22 21:46:31 -0500

    Add full highscores page w/ css
    Made js to get highscores from local storage, and then display the
    score, points and name of each score object. also did the css to style
    it nice and bright

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -1,11 +1,26 @@
 // retrieve the highscores array from localStorage, and output
 // it to the scores div
+var scoresArray = JSON.parse(localStorage.getItem('highscores'))
+console.log(scoresArray)
 
+// sort array by score, 
 // Display the highscores as an ordered list, along with a button to restart the game
 function displayHighscores() {
-    // get high score array from localStorage
+    // select highscores-here div
+    var scoresDiv = document.querySelector('.highscores-here');
 
-    // sort array in order of score
+    // loop over each score arrey element to display them
+    for (var index = 0; index < scoresArray.length; index++) {
+        // add elements to display score
+        // console.log(scoresArray[index].ini)
+        var scoreP = document.createElement("p")
+        // store score object properties as text
+        scoreP.innerText = scoresArray[index].initials + " | " + scoresArray[index].score + " | " + scoresArray[index].points;
+        // append p element to the div
+        scoresDiv.append(scoreP)
+    }
 
     // display an ordered list 
 }
+
+displayHighscores();
