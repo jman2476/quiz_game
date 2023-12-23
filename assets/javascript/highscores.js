72615f3fa4f48334f603ca500fce4f704d49c6df
Squirblechem <152367545+jman2476@users.noreply.github.com>
2023-12-22 21:59:04 -0500

    sorted the highscore array
    added code to sort the highscore array when bringing it out of
    localStorage

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -3,7 +3,11 @@
 var scoresArray = JSON.parse(localStorage.getItem('highscores'))
 console.log(scoresArray)
 
-// sort array by score, 
+// sort array by score, ignoring points, store as new variable
+scoresArray.sort((score1,score2) => (score1.score < score2.score) ? 1 : (score1.score > score2.score) ? -1 : 0);
+
+console.log(scoresArray)
+
 // Display the highscores as an ordered list, along with a button to restart the game
 function displayHighscores() {
     // select highscores-here div
